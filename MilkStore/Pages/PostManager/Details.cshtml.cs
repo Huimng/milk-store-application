6a71f10f142bd5136eac603095f8c191aee06010
ha <nguyenha24bmt@gmail.com>
2024-07-05 11:17:42 +0700

    ha/adjust comment for post manager

--- MilkStore/Pages/PostManager/Details.cshtml.cs
+++ MilkStore/Pages/PostManager/Details.cshtml.cs
@@ -4,33 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using BusinessObjects;
-using DAL;
 using BusinessLogics.Services;
-using DAL.Repository;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.SignalR;
+using MilkStore.Hubs;
 
 namespace MilkStore.Pages.PostManager
 {
     public class DetailsModel : PageModel
     {
-        private PostService postService;
-        private IProductService _productService;
-        private IAccountService accountService;
-        private CommentService commentService;
+        private readonly PostService postService;
+        private readonly IProductService _productService;
+        private readonly IAccountService accountService;
+        private readonly CommentService commentService;
+        private readonly IHubContext<ChatHub> _hub;
 
-        public DetailsModel(IServiceProvider serviceProvider)
+        public DetailsModel(IServiceProvider serviceProvider, IHubContext<ChatHub> hub)
         {
             postService = new PostService();
             _productService = serviceProvider.GetRequiredService<IProductService>();
             accountService = serviceProvider.GetRequiredService<IAccountService>();
             commentService = new CommentService();
+            _hub = hub;
         }
 
-      public Post Post { get; set; } = default!;
-        public IList<Comment> Comment { get; set; } = default!;
+        public Post Post { get; set; } = default!;
+        public IList<Comment> Comments { get; set; } = default!;
+        [BindProperty]
+        public Comment CommentIndex { get; set; } = new Comment();
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
@@ -40,14 +42,47 @@ namespace MilkStore.Pages.PostManager
                 return NotFound();
             }
             post.Product = _productService.GetProduct(post.ProductId);
-            
-            Post = postService.GetPost(id);
-            Comment = commentService.CommentInAPost(id);
-            foreach (var comment in Comment)
+
+            Post = post;
+            Comments = commentService.CommentInAPost(id);
+            foreach (var comment in Comments)
             {
                 comment.Post = postService.GetPost(comment.PostId);
             }
+
+            await _hub.Clients.All.SendAsync("Loading");
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int postId)
+        {
+            if (!ModelState.IsValid)
+            {
+                var post = postService.GetPost(postId);
+                if (post == null)
+                {
+                    return NotFound();
+                }
+                post.Product = _productService.GetProduct(post.ProductId);
+
+                Post = post;
+                Comments = commentService.CommentInAPost(postId);
+                foreach (var comment in Comments)
+                {
+                    comment.Post = postService.GetPost(comment.PostId);
+                }
+                return Page();
+            }
+
+            
+
+            CommentIndex.PostId = postId;
+            CommentIndex.CreateDate = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
+
+            commentService.Comment(CommentIndex);
+             _hub.Clients.All.SendAsync("Loading");
+
+            return RedirectToPage(new { id = postId });
         }
     }
 }
